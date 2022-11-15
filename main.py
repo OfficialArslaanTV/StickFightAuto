0169e7ed136b0126fb5463ea0a51d737025d3990
TheRealxMF <118288807+TheRealxMF@users.noreply.github.com>
2022-11-15 09:47:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from pynput.keyboard import Listener, KeyCode
 TOGGLE_KEY = KeyCode(char=";")
 EXIT_KEY = KeyCode(char="]")
 
-spamming = False
+clicking = False
 running = True
 
 
-def spammer():
+def clicker():
     while running:
-        if spamming:
-            pyautogui.press('c') # Replace C with your custom stick fight keybind if you have one.
+        if clicking:
+            pyautogui.press('c') #Replace "c" with your custom stick fight keybind if you have one.
         else:
             cache = ""
             cache.join(cache)
@@ -23,18 +23,20 @@ def spammer():
 
 def toggle_event(key):
     if key == TOGGLE_KEY:
-        global spamming
-        spamming = not spamming
+        global clicking
+        clicking = not clicking
     if key == EXIT_KEY:
-        spamming = False
+        global running
+        clicking = False
         running = False
         exit(0)
 
 
 print('To start/stop spamming, press ";"')
 print('To quit the program, press "]"')
-spam_thread = threading.Thread(target=spammer)
-spam_thread.start()
+click_thread = threading.Thread(target=clicker)
+click_thread.start()
 
 with Listener(on_press=toggle_event) as listener:
     listener.join()
+
